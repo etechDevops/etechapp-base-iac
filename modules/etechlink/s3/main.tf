34bcd1eee6f35743a203c46c5f1c97081d7043b7
Etech consulting <99599657+etechDevops@users.noreply.github.com>
2024-01-07 16:40:17 -0500

    Update main.tf

--- modules/etechlink/s3/main.tf
+++ modules/etechlink/s3/main.tf
@@ -1,1 +1,27 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.31.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = var.s3_bucket_name
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = (var.env == "prod")? true:false
+  }
+}
